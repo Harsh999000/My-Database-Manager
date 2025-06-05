82529963fe59999d08a5e72fcfba109a80ed8087
Harsh <jhaharsh101@gmail.com>
2025-06-06 03:59:44 +0530

    Adding Ngrok skip browser to skip the first warning page by ngrok for first time visitors on webpage

---
+++ src/main/java/com/myDatabaseManager/config/NgrokSkipBrowserWarningFilter.java
@@ -0,0 +1,22 @@
+package com.myDatabaseManager.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class NgrokSkipBrowserWarningFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        if (response instanceof HttpServletResponse) {
+            ((HttpServletResponse) response).setHeader("ngrok-skip-browser-warning", "true");
+        }
+
+        chain.doFilter(request, response);
+    }
+}
